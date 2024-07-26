b77c05b489215336f0be85e5cd56c2d4ec7c0de6
masabkhan-cyber <75547252+masabkhan-cyber@users.noreply.github.com>
2024-07-26 23:59:00 +0500

    minor fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,11 +59,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "MasabSK";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
+            document.title = "Come Back To Website";
             $("#favicon").attr("href", "assets/images/favhand.png");
         }
     });
